07b7480bf7f4d4771f63e5718490a8a8b2f7e947
Rolando704862 <morales.roland@live.com>
2018-11-08 23:08:45 -0600

    11/8

--- jm_bot.py
+++ jm_bot.py
@@ -39,47 +39,91 @@ kris_file_names = ['kris1','kris2']
 kris_index = 0
 
 @bot.command(pass_context = True)
-async def jm(ctx):
+async def menu(ctx,arg1='blank'):
+    chan = ctx.message.channel 
+    if arg1 == 'jm':
+        msg = 'For ' + arg1 + ' your options are: '
+        for entry in jm_file_names:
+            msg=msg+(entry + ", ")
+        msg = msg[0:len(msg)-2]
+        msg=msg+('.')
+    elif arg1 == 'cody':
+        msg = 'For ' + arg1 + ' your options are: '
+        for entry in cody_file_names:
+            msg=msg+(entry + ", ")
+        msg = msg[0:len(msg)-2]
+        msg=msg+('.')
+    elif arg1 == 'antonio':
+        msg = 'For ' + arg1 + ' your options are: '
+        for entry in antonio_file_names:
+            msg = msg+(entry + ", ")
+        msg=msg[0:len(msg)-2]
+        msg=msg+('.')
+    elif arg1 == 'kris': 
+        msg = 'For ' + arg1 + ' your options are: '
+        for entry in kris_file_names:
+            msg = msg+(entry + ", ")
+        msg = msg[0:len(msg)-2]
+        msg=msg+('.')
+        await bot.send_message(chan,msg)
+    else:
+        msg = 'You need to enter an argument (e.g. jm, cody, antonio, kris).'
+    await bot.send_message(chan,msg)
+
+@bot.command(pass_context = True)
+async def jm(ctx,arg1='blank'):
     global jm_index
     filename = folder_name + '/' + jm_file_names[jm_index]+'.mp3'
+    for name in jm_file_names:
+        if arg1 == name:
+            filename = 'soundfiles/'+name+'.mp3'
+            jm_index = jm_index-1
     senders_voice_chan = ctx.message.author.voice.voice_channel
     voice = await bot.join_voice_channel(senders_voice_chan) 
     player = voice.create_ffmpeg_player(filename)
+    player.volume = 2
     player.start()
-    time.sleep(MP3(filename).info.length)
+    time.sleep(MP3(filename).info.length+1)
     player.stop()
     await voice.disconnect()
     jm_index = jm_index + 1
     if jm_index == len(jm_file_names):
         jm_index = 0
 
-
-
 @bot.command(pass_context = True)
-async def antonio(ctx):
+async def antonio(ctx,arg1='blank'):
     global antonio_index
     filename = folder_name + '/' + antonio_file_names[antonio_index]+'.mp3'
+    for name in antonio_file_names:
+        if arg1 == name:
+            filename = 'soundfiles/'+name+'.mp3'
+            antonio_index = antonio_index-1
     senders_voice_chan = ctx.message.author.voice.voice_channel
     voice = await bot.join_voice_channel(senders_voice_chan) 
     player = voice.create_ffmpeg_player(filename)
+    player.volume = 2
     player.start()
-    time.sleep(MP3(filename).info.length)
+    time.sleep(MP3(filename).info.length+1)
     player.stop()
     await voice.disconnect()
     antonio_index = antonio_index + 1
     if antonio_index == len(antonio_file_names):
         antonio_index = 0
- 
 
 @bot.command(pass_context = True)
-async def cody(ctx):
+async def cody(ctx,arg1='blank'):
     global cody_index
     filename = folder_name + '/' + cody_file_names[cody_index]+'.mp3'
+    for name in cody_file_names:
+        if arg1 == name:
+            filename = 'soundfiles/'+name+'.mp3'
+            cody_index = cody_index-1
     senders_voice_chan = ctx.message.author.voice.voice_channel
     voice = await bot.join_voice_channel(senders_voice_chan) 
     player = voice.create_ffmpeg_player(filename)
+    player.volume = 2
     player.start()
-    time.sleep(MP3(filename).info.length)
+    time.sleep(MP3(filename).info.length+1)
     player.stop()
     await voice.disconnect()
     cody_index = cody_index + 1
@@ -87,14 +131,19 @@ async def cody(ctx):
         cody_index = 0
  
 @bot.command(pass_context = True)
-async def kris(ctx):
+async def kris(ctx,arg1='blank'):
     global kris_index
     filename = folder_name + '/' + kris_file_names[kris_index]+'.mp3'
+    for name in kris_file_names:
+        if arg1 == name:
+            filename = 'soundfiles/'+name+'.mp3'
+            kris_index = kris_index-1
     senders_voice_chan = ctx.message.author.voice.voice_channel
     voice = await bot.join_voice_channel(senders_voice_chan) 
     player = voice.create_ffmpeg_player(filename)
+    player.volume = 2
     player.start()
-    time.sleep(MP3(filename).info.length)
+    time.sleep(MP3(filename).info.length+1)
     player.stop()
     await voice.disconnect()
     kris_index = kris_index + 1
